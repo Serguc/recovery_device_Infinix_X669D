4008da4ce0d6da9c35eac1085a690d15ab974f52
serguc <42298249+Serguc@users.noreply.github.com>
2025-08-22 10:18:15 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Android device tree for infinix infinix X669D (infinix-X669D)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ device.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/infinix/X669D
+
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.0-impl-1.2
+
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.0-impl-default \
+    libhealthd.$(PRODUCT_PLATFORM)
+
+PRODUCT_PACKAGES += \
+    vendor.sprd.hardware.boot@1.2-impl \
+    vendor.sprd.hardware.boot@1.2-impl.recovery \
+    vendor.sprd.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl \
+    fastbootd
+
+# Trusty dependencies
+PRODUCT_PACKAGES += \
+    libtrusty \
+    libtrusty_metrics
