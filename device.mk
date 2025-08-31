ed1145f38866e60212f2601eda3ac94c1259bfb1
serguc <42298249+Serguc@users.noreply.github.com>
2025-08-31 10:21:41 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -8,11 +8,31 @@
 LOCAL_PATH := device/Infinix/X669D
 
 # VNDK
-PRODUCT_TARGET_VNDK_VERSION := 30
+PRODUCT_TARGET_VNDK_VERSION := 31
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Virtual_ab_ota
+AB_OTA_UPDATER := true
+ENABLE_VIRTUAL_AB := true
+#TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# AB
+AB_OTA_UPDATER := true
+
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    system \
+    product \
+    vendor \
+   
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -23,19 +43,18 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service \
-    android.hardware.boot@1.0-impl-1.2
+    android.hardware.boot@1.2-impl.recovery
 
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl
+
+# Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service \
     android.hardware.health@2.0-impl-default \
-    libhealthd.$(PRODUCT_PLATFORM)
 
-PRODUCT_PACKAGES += \
-    vendor.sprd.hardware.boot@1.2-impl \
-    vendor.sprd.hardware.boot@1.2-impl.recovery \
-    vendor.sprd.hardware.boot@1.2-service
-
+# Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl \
     fastbootd
